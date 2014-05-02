0b4adc11ca7ae83a28b6cef5c056c036cd2f8f85
rdouglas <doug3230@mylaurier.ca>
2014-05-02 15:08:38 -0400

    Edited navigationzone
    -added a note describing it,
    -surrounded it with a blue border,
    -noted the possible need for some client-side scripting.

--- Prototype/includes/navigationzone.html.php
+++ Prototype/includes/navigationzone.html.php
@@ -1,11 +1,14 @@
 <div id="navigationzone">
 	<ul>
+		<li>Here is the navigation zone. 
+			It can be used to navigate the site quickly.</li>
 		<li><a href="index.php">Home</a></li>
 		<li><a href="bands.php">Bands</a></li>
 		<li><a href="albums.php">Albums</a></li>
 		<li><a href="songs.php">Songs</a></li>
 		<li><a href="archives.php">Archives</a></li>
-		<li><strong>Important:</strong> Get the navigation area 
-			looking stylish. Perhaps a horizontal dropdown.</li>
+		<li><strong>Important:</strong> The navigation area 
+			looks stylish. Perhaps by using a horizontal 
+			dropdown layout (Client-side scripting would be required).</li>
 	</ul>
 </div> <!-- End of navigationzone div -->
