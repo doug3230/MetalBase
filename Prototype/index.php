04363c16ae80c3db3b9b6f6983cd71567beb6a16
rdouglas <doug3230@mylaurier.ca>
2014-05-01 20:31:24 -0400

    Added description to homepage

--- Prototype/index.php
+++ Prototype/index.php
@@ -10,5 +10,8 @@
 		<?php include 'includes/navigationzone.html.php'; ?>
 		
 		<h2>Welcome to the MetalBase Website!</h2>
+		<p>Here I describe the website, what it is for, and how to use it.</p>
+		<p>This page serves as an "About" page, and does not need to be dynamically changed over time.</p>
+		<p><strong>Optional:</strong> this page can include links to favourite parts of the site.</p>
 	</body>
 </html>
