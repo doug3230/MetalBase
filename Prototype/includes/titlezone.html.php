f3016d9bbf2bd4331e048e2af35566ff99cb8732
rdouglas <doug3230@mylaurier.ca>
2014-05-02 15:16:54 -0400

    Edited titlezone
    -added description,
    -added border,
    -added small gap between titlezone and navigationzone.

--- Prototype/includes/titlezone.html.php
+++ Prototype/includes/titlezone.html.php
@@ -1,10 +1,11 @@
 <div id="titlezone">
+	<p>Here is the title zone. It appears at the top of each page.</p>
 	<div id="titlezoneheading">
 		<h1><a href="index.php" class="nolinkbody">MetalBase</a></h1>
 	</div> <!-- End of titleheading div -->
 	<div id="titlezoneimage">
 		<p><em><a href="index.php" class="nolinkbody">
-			Here there is an image. Both the site title and image 
+			Here there is also an image. Both the site title and image 
 			link back to the homepage when clicked.</a></em></p>
 	</div> <!-- End of titlezoneimage div -->
 </div> <!-- End of titlezone div -->
