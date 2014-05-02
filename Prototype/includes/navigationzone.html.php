acbcf7a8710599c853452278dd3306f7fda4d568
rdouglas <doug3230@mylaurier.ca>
2014-05-01 20:25:03 -0400

    Removed Requirements references from the prototype's navigationzone
    In addition, the home and about us pages are combined into one.

--- Prototype/includes/navigationzone.html.php
+++ Prototype/includes/navigationzone.html.php
@@ -1,14 +1,10 @@
 <div id="navigationzone">
 	<ul>
-		<p>Required Pages</p>
-		<li><a href="index.php">Home</a></li> 
-		<li><a href="about.php">About</a></li>
+		<li><a href="index.php">Home</a></li>
 		<li><a href="bands.php">Bands</a></li>
 		<li><a href="albums.php">Albums</a></li>
-		<p>Optional Pages</p>
 		<li><a href="songs.php">Songs</a></li>
 		<li><a href="images.php">Images</a></li>
 		<li><a href="archives.php">Archives</a></li>
-		<p>Done Pages</p>
 	</ul>
 </div> <!-- End of navigationzone div -->
