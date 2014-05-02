4cf288becb89c91ef058ba6e83009835516c45ab
rdouglas <doug3230@mylaurier.ca>
2014-05-02 15:00:26 -0400

    Edited titlezone
    -changed wording,
    -added links,
    -removed tagline.

--- Prototype/includes/titlezone.html.php
+++ Prototype/includes/titlezone.html.php
@@ -1,13 +1,10 @@
 <div id="titlezone">
 	<div id="titlezoneheading">
-		<h1>MetalBase</h1>
+		<h1><a href="index.php" class="nolinkbody">MetalBase</a></h1>
 	</div> <!-- End of titleheading div -->
 	<div id="titlezoneimage">
-		<p><em>I am an image for the website. 
-			I am not text! I link you back to 
-			the homepage if you click me.</em></p>
+		<p><em><a href="index.php" class="nolinkbody">
+			Here there is an image. Both the site title and image 
+			link back to the homepage when clicked.</a></em></p>
 	</div> <!-- End of titlezoneimage div -->
-	<div id="titlezonetagline">
-		<h2>Your Heavy Metal Database</h2>
-	</div> <!-- End of titletagline div -->
 </div> <!-- End of titlezone div -->
