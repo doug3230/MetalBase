8aa0ce82f0ae876aecb51078f2b307d3d7b2335e
rdouglas <doug3230@mylaurier.ca>
2014-05-02 15:28:51 -0400

    Edited footerzone
    -edited description,
    -added possibility of including an image here.

--- Prototype/includes/footerzone.html.php
+++ Prototype/includes/footerzone.html.php
@@ -1,5 +1,7 @@
 <div id="footerzone">
-	<p>Here I write something about how MetalBase is my creation.</p>
+	<p>Here is the footer zone. It appears at the bottom of each page.</p>
+	<p>Here the user reads something indicating that MetalBase is my creation.</p>
+	<p><strong>Optional:</strong> Include an appropriate image.</p>
 	<p><strong>Optional:</strong> Include a method by which users could 
 		theoretically contact me if something goes wrong.</p>
 </div> <!-- End of footerzone div -->
