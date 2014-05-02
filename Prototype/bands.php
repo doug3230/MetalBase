eee122b6428080f59faee4ccfb358ca582750ac7
rdouglas <doug3230@mylaurier.ca>
2014-05-01 21:55:27 -0400

    Added description to bands page

--- Prototype/bands.php
+++ Prototype/bands.php
@@ -10,6 +10,20 @@
 		<?php include 'includes/navigationzone.html.php'; ?>
 		
 		<h2>Bands on the Site</h2>
+		<form><input type="text" /><input type="submit" value="Search bands" /></form>
+		<p>Here I give the option to give a detailed search for a band with a name similar to the input text. <br />
+			I allow the user to filter search results by genre, country, (<strong>Optional:</strong> and the 
+			letter of the alphabet that the band's name starts with).</p>
+		
+		<p>I also provide links to individual pages for viewing
+			<ul>
+				<li>bands by the letter of the alphabet that their name starts with</li>
+				<li>bands by genre</li>
+				<li>bands by country</li>
+			</ul>
+			<strong>Optional:</strong> Have the 3 above subpages be integrated into the navigation zone in a
+			manner that is pleasing to the user (a dropdown menu perhaps).</p>
+		<p>This page also provides a link to the "Add a Band" page.</p>
 		
 		<?php include 'includes/footerzone.html.php'; ?>
 	</body>
