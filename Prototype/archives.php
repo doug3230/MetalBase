48a9a22acfe3deb99973a3b150f5060217956193
rdouglas <doug3230@mylaurier.ca>
2014-05-01 20:54:30 -0400

    Added description to archives page

--- Prototype/archives.php
+++ Prototype/archives.php
@@ -10,6 +10,11 @@
 		<?php include 'includes/navigationzone.html.php'; ?>
 		
 		<h2>External Archives</h2>
+		<p>Here I provide some links to online resources I find useful.</p>
+		<p>Examples of external archives are 
+		<a href="http://www.metal-archives.org">metal-archives.org</a> 
+		and <a href="http://www.wikipedia.org">Wikipedia</a>.</p>
+		<p><strong>Optional:</strong> This page can provide a short description of each resource.</p>
 		
 		<?php include 'includes/footerzone.html.php'; ?>
 	</body>
