8dcfbe65d724a158026aa84c8ea59a9210265088
rdouglas <doug3230@mylaurier.ca>
2014-05-02 12:39:27 -0400

    Added an important note to the Prototype navigation zone

--- Prototype/includes/navigationzone.html.php
+++ Prototype/includes/navigationzone.html.php
@@ -5,5 +5,7 @@
 		<li><a href="albums.php">Albums</a></li>
 		<li><a href="songs.php">Songs</a></li>
 		<li><a href="archives.php">Archives</a></li>
+		<li><strong>Important:</strong> Get the navigation area 
+			looking stylish. Perhaps a horizontal dropdown.</li>
 	</ul>
 </div> <!-- End of navigationzone div -->
