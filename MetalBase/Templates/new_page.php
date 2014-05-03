ef6d92b996c6115b0deed4135677b6517c1cf909
rdouglas <doug3230@mylaurier.ca>
2014-05-03 13:04:08 -0400

    Created new page template for Implementation

---
+++ MetalBase/Templates/new_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include 'Includes/headcontent.inc.php'; ?>
+		<title>MetalBase - Your Heavy Metal Database</title>
+	</head>
+	<body>
+		<?php include 'Includes/titlezone.inc.php'; ?>
+		
+		<?php include 'Includes/navigationzone.inc.php'; ?>
+		
+		<div id="contentzone">
+			<h3>Here is the content zone. Its purpose varies from page to page.</h3>
+			<h2>Write Stuff Here!</h2>
+		</div> <!-- End of contentzone div -->
+		
+		<?php include 'Includes/footerzone.inc.php'; ?>
+	</body>
+</html>
