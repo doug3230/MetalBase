5df573bc04efcfa32d8904665e922fe64a925c78
rdouglas <doug3230@mylaurier.ca>
2014-05-02 21:07:26 -0400

    Added small gap between search text and button

--- Prototype/includes/searchpage.html.php
+++ Prototype/includes/searchpage.html.php
@@ -13,7 +13,8 @@
 			<h3>Here is the content zone. Its purpose varies from page to page.</h3>
 			<h2><?php echo ucfirst($element);?>s on the Site</h2>
 			<p><form action="results.php">
-				<input type="text" /><input type="submit" value="<?php echo 'Search ' . ucfirst($element) . 's';?>" />
+				<input type="text" />
+				<input type="submit" value="<?php echo 'Search ' . ucfirst($element) . 's';?>" />
 			</form></p>
 			<p>Here the user can perform a detailed search for <?php echo $element;?>s with a name that includes the input text. <br />
 				The user is able to filter search results by genre, country, (<strong>Optional:</strong> and the 
