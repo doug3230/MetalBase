60cfa7c8c58abebefb164f60e8e90356db7c5159
rdouglas <doug3230@mylaurier.ca>
2014-05-03 13:01:16 -0400

    Revert "Created fullPathTo function for making includes easier"
    This reverts commit 9e92d9cd880b4b7aebbdd2686b04f5dc27553ea6.

--- MetalBase/Functions/general_utilities.php
+++
@@ -1,7 +0,0 @@
-<?php
-function fullPathTo($locationInMetalBaseFolder) {
-	$rootLocation = $_SERVER['DOCUMENT_ROOT'] . '/MetalBase/';
-	$full_path = $rootLocation . $locationInMetalBaseFolder;
-	return $full_path;
-}
-?>
