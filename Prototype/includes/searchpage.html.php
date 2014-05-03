4517b600cfdab93a1f9027521bcb611e53225cb3
rdouglas <doug3230@mylaurier.ca>
2014-05-02 21:03:57 -0400

    Edited search pages so that they send you to the results page when the form is submitted

--- Prototype/includes/searchpage.html.php
+++ Prototype/includes/searchpage.html.php
@@ -12,7 +12,9 @@
 		<div id="contentzone">
 			<h3>Here is the content zone. Its purpose varies from page to page.</h3>
 			<h2><?php echo ucfirst($element);?>s on the Site</h2>
-			<form><input type="text" /><input type="submit" value="<?php echo 'Search ' . ucfirst($element) . 's';?>" /></form>
+			<p><form action="results.php">
+				<input type="text" /><input type="submit" value="<?php echo 'Search ' . ucfirst($element) . 's';?>" />
+			</form></p>
 			<p>Here the user can perform a detailed search for <?php echo $element;?>s with a name that includes the input text. <br />
 				The user is able to filter search results by genre, country, (<strong>Optional:</strong> and the 
 				letter of the alphabet that the <?php echo $element;?>'s name starts with).</p>
