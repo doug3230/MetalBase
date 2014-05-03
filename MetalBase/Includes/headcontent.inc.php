0bf7c580eca88ac1a6d2c51f53786adf8304f420
rdouglas <doug3230@mylaurier.ca>
2014-05-03 12:25:43 -0400

    Created Implementation headcontent include file

---
+++ MetalBase/Includes/headcontent.inc.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8"
+	http-equiv="Content-Type"
+	content="text/html; charset=utf-8"  />
+<link href="<?php echo "$_SERVER['DOCUMENT_ROOT']/Styles/style.css";?>" rel="stylesheet" type="text/css" />
