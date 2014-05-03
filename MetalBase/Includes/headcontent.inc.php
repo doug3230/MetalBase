4fb87b7fc62cb98d737323ca4a5c5e4f63b631ae
rdouglas <doug3230@mylaurier.ca>
2014-05-03 12:38:33 -0400

    Corrected a minor error in the headcontent include

--- MetalBase/Includes/headcontent.inc.php
+++ MetalBase/Includes/headcontent.inc.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8"
 	http-equiv="Content-Type"
 	content="text/html; charset=utf-8"  />
-<link href="<?php echo "$_SERVER['DOCUMENT_ROOT']/Styles/style.css";?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo $_SERVER['DOCUMENT_ROOT'] . 
+	'/MetalBase/Styles/style.css';?>" rel="stylesheet" type="text/css" />
