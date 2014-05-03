c34783af9cd9efd3754d9a35978d5cecd303c48e
rdouglas <doug3230@mylaurier.ca>
2014-05-03 13:05:58 -0400

    Removed remnants of fullPathTo function from head content include

--- MetalBase/Includes/headcontent.inc.php
+++ MetalBase/Includes/headcontent.inc.php
@@ -1,5 +1,4 @@
 <meta charset="utf-8"
 	http-equiv="Content-Type"
 	content="text/html; charset=utf-8"  />
-<link href="<?php echo $_SERVER['DOCUMENT_ROOT'] . 
-	'/MetalBase/Styles/style.css';?>" rel="stylesheet" type="text/css" />
+<link href="Styles/style.css" rel="stylesheet" type="text/css" />
