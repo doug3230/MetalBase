d2edc86fe9dfdb6d303326511e169984a8910d02
rdouglas <doug3230@mylaurier.ca>
2014-05-07 14:35:56 -0400

    Created include for choosing 'band', 'album', or 'song'

---
+++ MetalBase/Includes/choose_type.inc.php
@@ -0,0 +1,9 @@
+<h3>Select the type of element to search for: </h3>
+<form method="get" action="">
+	<select name="type" id="type">
+		<option value="band">Band</option>
+		<option value="album">Album</option>
+		<option value="song">Song</option>
+	</select>
+	<input type="submit" value="Submit" />
+</form>
