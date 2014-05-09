87051635e3d6e49cf92cd1360db70ceb7c233757
rdouglas <doug3230@mylaurier.ca>
2014-05-08 22:02:50 -0400

    simplified genre and country insertions by using the library

--- MetalBase/Includes/countries/add_country_if_POST.inc.php
+++ MetalBase/Includes/countries/add_country_if_POST.inc.php
@@ -1,17 +1,8 @@
 <?php
-try {
-	if(isset($_REQUEST['new_country'])) {
-		$sql = "INSERT INTO country (name) VALUES (:country_name)";
-		$s = $pdo->prepare($sql);
-		
-		$country_name = $_REQUEST['new_country'];
-		$s->bindvalue(':country_name', $country_name);
-		$s->execute();
-		echo "<h3>$country_name successfully added to MetalBase.</h3>";
-	}
-} catch (Exception $e) {
-	echo '<p>Error adding new country: ' . $e->getMessage() . '</p>';
-	include 'Includes/error.inc.php';
-	exit();
+if(isset($_REQUEST['new_country'])) {
+	include_once "Includes/insert_lib.inc.php";
+	$country_name = $_REQUEST['new_country'];
+	insert_country($country_name);
+	echo "<h3>$country_name successfully added to MetalBase.</h3>";
 }
 ?>
--- MetalBase/Includes/genres/add_genre_if_POST.inc.php
+++ MetalBase/Includes/genres/add_genre_if_POST.inc.php
@@ -1,17 +1,8 @@
 <?php
-try {
-	if(isset($_REQUEST['new_genre'])) {
-		$sql = "INSERT INTO Genre (name) VALUES (:genre_name)";
-		$s = $pdo->prepare($sql);
-		
-		$genre_name = $_REQUEST['new_genre'];
-		$s->bindvalue(':genre_name', $genre_name);
-		$s->execute();
-		echo "<h3>$genre_name successfully added to MetalBase.</h3>";
-	}
-} catch (Exception $e) {
-	echo '<p>Error adding new genre: ' . $e->getMessage() . '</p>';
-	include 'Includes/error.inc.php';
-	exit();
+if(isset($_REQUEST['new_genre'])) {
+	include_once "Includes/insert_lib.inc.php";
+	$genre_name = $_REQUEST['new_genre'];
+	insert_genre($genre_name);
+	echo "<h3>$genre_name successfully added to MetalBase.</h3>";
 }
 ?>
