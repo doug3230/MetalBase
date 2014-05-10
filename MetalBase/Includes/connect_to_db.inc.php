476835428e62ab940faf603e2766c5451e3405ee
rdouglas <doug3230@mylaurier.ca>
2014-05-10 15:39:14 -0400

    Changed password to one that is more generic

--- MetalBase/Includes/connect_to_db.inc.php
+++ MetalBase/Includes/connect_to_db.inc.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$pdo = new PDO('mysql:host=localhost;dbname=metalbase', 'mbuser', 'crazyperson');
+	$pdo = new PDO('mysql:host=localhost;dbname=metalbase', 'mbuser', 'mbpassword');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
 	echo '<p>Error connecting to the database server: ' . $e->getMessage() . '</p>';
